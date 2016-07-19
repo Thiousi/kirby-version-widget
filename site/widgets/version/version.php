b2db2052453bee25024c678d48488c54bdb56fb5
Mathieu Etienne <thiousi@gmail.com>
2016-07-19 22:42:08 +0200

    Cleaned up and documented the code

--- site/widgets/version/version.php
+++ site/widgets/version/version.php
@@ -1,27 +1,28 @@
 <?php
-
-
-
 return array(
-		'options' => array(
-		      array(
-		        'text' => 'View Changelog',
-		        'icon' => 'version',
-		        'link' => 'http://getkirby.com/changelog',
-		        'target' => '_blank'
-		      )
-		    ),
-		'title' => 'Kirby Version',
-		'html'  => function() {
-		// any data for the template
-		$kirby = kirby();
-		$rss = simplexml_load_file('http://getkirby.com/changelog/feed');
-		$data = array(
-			'kirby' => $kirby::$version,
-			'latest' => $rss->channel->item[0]->title,
-			'link' => $rss->channel->item[0]->link,
-		);
-
-		return tpl::load(__DIR__ . DS . 'template.php', $data);
-		}
-		);
+  	// Defines the header of the widget
+	'options' => array(
+	      array(
+	        'text' => 'View Changelog',
+	        'icon' => 'version',
+	        'link' => 'http://getkirby.com/changelog',
+	        'target' => '_blank'
+	      )
+	    ),
+    
+  	// Defines the title of the widget
+	'title' => 'Kirby Version',
+  
+  	// Defines the core content of the widget
+	'html'  => function() {
+	  	$kirby = kirby(); // Get current version from Kirby
+	  	$rss = simplexml_load_file('http://getkirby.com/changelog/feed'); 
+	  	$data = array(
+	  		'kirby' => $kirby::$version,
+	  		'latest' => $rss->channel->item[0]->title, // Check the version number of the latest entry in the Kirby changelog
+	  		'link' => $rss->channel->item[0]->link, // Get the link to the latest entry in the Kirby changelog
+	  	);
+		// Return 'options', 'title', and 'html' to the template
+	  	return tpl::load(__DIR__ . DS . 'template.php', $data);
+	}
+);
