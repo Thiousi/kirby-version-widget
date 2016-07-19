dbddc681585bc3ce9d7f0a01e67c9df8f86f2a10
Mathieu Etienne <thiousi@gmail.com>
2016-07-19 22:56:10 +0200

    Added if else statement

--- site/widgets/version/template.php
+++ site/widgets/version/template.php
@@ -1,4 +1,10 @@
 <ul class="nav nav-list sidebar-list">
 	<li><strong>Installed:</strong> Kirby <?php echo $kirby; ?></li>
-	<li><a href=<?php echo $link; ?> style="padding-left: 0"><strong>Latest:</strong> <?php echo $latest; ?></a></li>
+	<li>
+	<?php if(substr($latest, 6) == $kirby) {
+		echo '<strong style= "color: #1abc9c">You have the latest version installed!</strong>';
+	} else {
+		echo '<a href="' . $link . '" style="padding-left: 0; color: #e90000"><strong>New version available: </strong>' . $latest . '</a>';
+	}; ?>
+	</li>
 </ul>
