4578f4ac75a174776a6d0539902ae3633e74baea
Mathieu Etienne <thiousi@gmail.com>
2016-08-27 18:13:58 +0200

    Added custom cache

--- version/template.php
+++ version/template.php
@@ -1,10 +1,10 @@
 <ul class="nav nav-list sidebar-list">
 	<li style="margin-bottom: 12px"><strong>Installed:</strong> Kirby <?php echo $kirby; ?></li>
 	<li>
-	<?php if(substr($latest, 6) == $kirby):
+	<?php if($latest == $kirby):
 		echo '<strong style="color: #8dae28">You have the latest version installed!</strong>';
 	else:
-		echo '<a href="' . $link . '" style="padding-left: 0; color: #b3000a"><strong>New version available: </strong>' . $latest . ' <i class="icon fa fa-chain"></i></a>';
+		echo '<i class="fa fa-chain"></i> <a href="' . $link . '" style="padding-left: 0; color: #b3000a"><strong> New version available: </strong> Kirby ' . $latest . ' </a>';
 	endif; ?>
 	</li>
 </ul>
